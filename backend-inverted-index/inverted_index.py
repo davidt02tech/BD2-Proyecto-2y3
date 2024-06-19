408976c3ddf2d33c5058b1323727fe5b89007857
leonardocandio <leonardo.candio@utec.edu.pe>
2024-06-18 23:59:03 -0500

    spimi

--- backend-inverted-index/inverted_index.py
+++ backend-inverted-index/inverted_index.py
@@ -4,32 +4,59 @@ import heapq
 import pandas as pd
 import os
 
+def make_tuple(string):
+    first, second = string.split(',', 1)
+    first = first[1:]
+    second = second[:-1]
+    return first, int(second)
+
+
 class Index:
     pass
 
 class Bucket:
-    def __init__(self, postings=set, tf=0, next_bucket=-1, bucket_size=0):
+    BUCKET_SIZE = 4096
+    id = 0
+    def __init__(self, folder_path, postings=defaultdict(int), next_bucket=None):
+        self.id = Bucket.id
+        self.path = folder_path
+        Bucket.id += 1
         self.postings = postings
-        self.tf = tf
         self.next_bucket = next_bucket
-        self.bucket_size = bucket_size
+        self.first_bucket = self.id
+        self.size = 0
 
-    def __repr__(self):
-        return f"({self.postings}, {self.tf}, {self.next_bucket}, {self.bucket_size})"
+    def serialize(self):
+        return f"({dict.__repr__(self.postings)}, {self.next_bucket})"
 
-    def _get_len(self):
-        return len(self.__repr__())
+    def get_path(self):
+        return os.path.join(self.path, self.get_pointer())
+
+    def get_pointer(self):
+        return f"bucket_{self.first_bucket}.txt"
 
     def add_to_postings(self, doc_id):
-        pass
+        if doc_id not in self.postings.keys() and len(self.postings) + 1 > Bucket.BUCKET_SIZE:
+            self.save_bucket()
+            self.id = Bucket.id + 1
+            Bucket.id += 1
+            self.postings = defaultdict(int)
+            self.next_bucket = None
+        self.postings[doc_id] += 1
+        self.size += 1
+
+    def save_bucket(self):
+        self.next_bucket = Bucket.id + 1
+        with open(self.get_path(), "w") as f:
+            f.write(self.serialize())
 
 class IndexEntry:
-    def __init__(self, term, doc_ids):
+    def __init__(self, term, bucket_pointer):
         self.term = term
-        self.doc_ids = doc_ids
+        self.bucket_pointer = doc_ids
 
     def __str__(self):
-        return f"{self.term}:{self.doc_ids}"
+        return f"{self.term}:{self.bucket_pointer}"
 
     def __lt__(self, other):
         return self.term < other.term
@@ -41,11 +68,7 @@ class IndexEntry:
 
 class SPIMI(Index):
     BLOCK_SIZE = 4096  # n of entries
-    BUCKET_SIZE = 4096  # n of chars
     index_number = 0
-
-    def get_buckets_path(self):
-        return os.path.join(self.path, f"buckets_{self.index_number}.txt")
 
     def get_block_path(self, current_index=None):
         if current_index is None:
@@ -53,7 +76,6 @@ class SPIMI(Index):
         return os.path.join(self.path, f"index_{self.index_number}_block_{current_index}.txt")
 
     def __init__(self, data_path, index_folder):
-        self.index = defaultdict(Bucket)
         self.path = index_folder
         if not os.path.exists(self.path):
             os.makedirs(self.path)
@@ -61,70 +83,106 @@ class SPIMI(Index):
         self.current_index = 0
         self.index_number = SPIMI.index_number
         SPIMI.index_number += 1
+        self.index = defaultdict(self._get_empty_bucket)
+
+    def _get_empty_bucket(self):
+        return Bucket(self.path)
 
     def add(self, term, doc_id):
-        if len(self.index) + 1 > self.BLOCK_SIZE:
+        if len(self.index) + 1 > SPIMI.BLOCK_SIZE:
             self.save_block()
-            self.index = defaultdict(Bucket)
+            self.index = defaultdict(self._get_empty_bucket)
+            self.current_index += 1
         self.index[term].add_to_postings(doc_id)
 
     def get(self, term):
         return self.index.get(term, {})
 
-    def save_block(self):
-        self.index = dict(sorted(self.index.items()))
+    def save_block(self, sort=True):
+        if sort:
+            self.index = dict(sorted(self.index.items()))
         with open(self.get_block_path(), 'w', encoding="utf-8") as f:
             for term, bucket in self.index.items():
-                f.write(f"{term}:{str(bucket)}")
+                f.write(f"({term},{bucket.size}):{bucket.get_pointer()}\n")
 
-        self.index = defaultdict(set)
+        self.index = defaultdict(self._get_empty_bucket)
 
     def load(self, path):
         with open(path, 'r', encoding='utf-8') as f:
             for line in f:
                 term, (df, doc_ids) = line.split(":", 1)
                 self.index[term.strip()] = eval(doc_ids)
+    def load_bucket(self, bucker_pointer):
+        with open(os.path.join(self.path, bucket_pointer),'r') as f:
+            line = f.readline()
+            postings, next = eval(line)
+        return posting, next
+
 
     def build(self):
-        # build step
+        build step
         data = pd.read_csv(self.file)
         data = data[data["language"].isin(["en", "es", "de"])]
         current_id = 0
         for index, row in data.iterrows():
-            tokens = process_text(row["lyrics"], row["language"])
+            tokens = process_text(row["lyrics"] + " " + row["track_name"] + " " + row["track_artist"], row["language"])
             for token in tokens:
                 self.add(token, index)
         # merge step
-        self.save(os.path.join(self.path,
-                               f"index_{self.index_number}_file_{self.current_index}.txt"))
-        self.current_index += 1
+        self.save_block()
         sorted_index = []
-        merged_index = defaultdict(set)
-        file_names = [os.path.join(self.path, f"index_{self.index_number}_file_{index}.txt")
-                      for index in range(self.current_index)]
+        file_names = [self.get_block_path(current_index=i) for i in range(self.current_index + 1)]
 
-        readers = [open(file, 'r', encoding="utf-8") for file in file_names]
+        merged = dict()
+        readers = [(i, open(file, 'r', encoding="utf-8")) for i, file in enumerate(file_names)]
+        empty_readers = set()
 
-        while readers:
-            for i, reader in enumerate(readers):
-                line = reader.readline()
-                if not line:
-                    readers.pop(i)
-                    continue
-                term, doc_ids = line.split(":", 1)
-                heapq.heappush(sorted_index, IndexEntry(term, eval(doc_ids)))
-        for entry in sorted_index:
-            merged_index[entry.term].add(entry.doc_ids)
+        curr_reader = int()
 
-        # save merged index and delete blocks
-        for reader in readers:
-            reader.close()
-        for file in file_names:
+        for i, reader in readers:
+            line = reader.readline()
+            term_df, bucket_pointer = line.split(":", 1)
+            term, df = make_tuple(term_df)
+            heapq.heappush(sorted_index, (term, (df, bucket_pointer, curr_reader)))
+
+
+        merged_index = 0
+
+        while len(empty_readers) < len(file_names):
+            term, (df, bucket_pointer, curr_reader) = heapq.heappop(sorted_index)
+            if len(merged) + 1 > self.BLOCK_SIZE:
+                with open(os.path.join(self.path, f"index_{self.index_number}_merged_{merged_index}.txt"), 'w', encoding="utf-8") as f:
+                    for term, bucket in merged:
+                        f.write(f"({term},{df}):{bucket_pointer}\n")
+                merged = dict()
+
+            if term in merged:
+                next = merged[term]
+                while next is not None: 
+                    temp = next
+                    postings, next = self.load_bucket(next)
+                with open(os.path.join(self.path, temp), "r") as f:
+                    f.write(f"({postings},{bucket_pointer})")
+            else:
+                merged[term] = bucket_pointer
+
+            pos, reader = readers[curr_reader]
+            if pos in empty_readers:
+                continue
+            line = reader.readline()
+            if not line:
+                empty_readers.add(pos)
+                reader.close()
+                continue
+            term_df, bucket_pointer = line.split(":", 1)
+            term, df = make_tuple(term_df)
+            heapq.heappush(sorted_index, (term, (df, bucket_pointer, curr_reader)))
+
+
+        # delete unmerged indexes
+        for i in range(self.current_index + 1):
+            file = self.get_block_path(i)
             os.remove(file)
-        with open(os.path.join(self.path, f"merged_index_{self.index_number}.txt"), encoding="utf-8") as f:
-            for term, doc_ids in merged_index.items():
-                f.write(f"{term}:{doc_ids}\n")
-
 
     def search_query_in_index(self, query):
         with open(os.path.join(self.path, f"merged_index_{self.index_number}.txt"), 'r', encoding="utf-8") as f:
